c7dbc3b22f58a564ba61bb25f6cce9b535f3737f
notthegit <tossapol29054@gmail.com>
2017-12-07 22:33:07 +0700

    Fix facebook login

--- Commander/app/src/main/java/notthegit/commander/kmitl/commander/MainActivity.java
+++ Commander/app/src/main/java/notthegit/commander/kmitl/commander/MainActivity.java
@@ -1,9 +1,11 @@
 package notthegit.commander.kmitl.commander;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -14,6 +16,8 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
 
 import org.json.JSONObject;
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     LoginButton loginButton;
     CallbackManager callbackManager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
         //loginButton.setReadPermissions(Arrays.asList("name", "email"));
+        final Button post = findViewById(R.id.post_btn);
+        post.setVisibility(View.GONE);
 
 
 
@@ -55,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                         //parameters.putString("fields", "name,email");
                         //request.setParameters(parameters);
                         //request.executeAsync();
+                        post.setVisibility(View.VISIBLE);
                     }
 
                     @Override
@@ -83,10 +91,20 @@ public class MainActivity extends AppCompatActivity {
         Intent level = new Intent(this,HelpActivity.class);
         startActivity(level);
     }
+    public void clickPost(View view){
+        //Bitmap image = getDrawable(R.drawable.logo);
+        //SharePhoto photo = new SharePhoto.Builder()
+        //        .setBitmap(image)
+        //        .build();
+        //SharePhotoContent content = new SharePhotoContent.Builder()
+        //        .addPhoto(photo)
+        //        .build();
+    }
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
     }
 }
 
